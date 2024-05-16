2424f3b7263fd9e51d69b43d34a2f7e365eb3e25
enemuo <francenemuo@gmail.com>
2024-05-16 15:15:06 +0100

    feat: add related sections

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,8 +1,11 @@
 import { data } from "@/app/components/data"
 import Image from "next/image"
+import Link from "next/link"
 
 export default function ProductPage({params}) {
     const filteredProduct = data.find(product => product.name.split(' ').join('-') == params.id)
+
+    const relatedProducts = data.filter(product =>  product.name !== filteredProduct.name && product.type == filteredProduct.type).slice(0, 6)
 
   return (
     <div className="px-16 py-12 " >
@@ -11,6 +14,7 @@ export default function ProductPage({params}) {
         <div>
 
         <h2 className="font-semibold text-3xl pb-6">{filteredProduct.name}</h2>
+        <h3><span className="font-bold">Region:</span> {filteredProduct.region.join(', ')}</h3>
         {filteredProduct.description.map(para => (
             <p key={Math.random()} className="text-lg py-2">{para}</p>
         ))}
@@ -32,7 +36,23 @@ export default function ProductPage({params}) {
 
         <div>
             <h2 className="pt-10 text-4xl">Related Products</h2>
+
+            <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3"> 
+                {relatedProducts.map(datum =>
+                    (
+                        <section key={datum.id}  className="flex flex-col gap-3 items-center shadow-xl rounded-2xl py-10 text-2xl">
+                          <Link href={`/product/${datum.name.split(' ').join('-')}`} title={datum.name}>
+                          <Image src={`/cakes/${datum.name.split(' ').join('-')}.jpg`} alt={datum.name} width='300' height='400' className="h-40 w-40" />
+                          <h3>{datum.name}</h3>
+                          <p className="bg-fuchsia-500 py-2 px-8 rounded-full"><span>&#8358;</span><span>{datum.amount}</span></p>
+                          </Link>
+                        </section>
+                      ))
+                }
+            </div>
+
         </div>
+
     </div>
   )
 }
