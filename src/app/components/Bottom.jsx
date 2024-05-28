a3f0f07d4dcb46277d55be41b8ac0fac9559c8e2
enemuo <francenemuo@gmail.com>
2024-05-28 16:05:22 +0100

    fix: add padding to right

--- src/app/components/Bottom.jsx
+++ src/app/components/Bottom.jsx
@@ -13,7 +13,7 @@ export default function Bottom() {
 
     const cartVal = 0
   return (
-    <nav className='px-16 bg-fuchsia-500 text-white flex sm:flex-row flex-col justify-between items-center'>
+    <nav className='pl-16 bg-fuchsia-500 text-white flex sm:flex-row flex-col justify-between items-center'>
         <div className='flex flex-row justify-between items-center sm:gap-0 gap-[300px]'>
 
         <ul className='flex flex-row justify-around items-center h-[10vh]'>
@@ -37,7 +37,7 @@ export default function Bottom() {
             <li><Link href='/contacts' className=''>Contacts</Link></li>
           
            <li>
-             <ul className='flex gap-4 items-center justify-end sm:pb-0 pb-3 text-lg cursor-pointer sm:pr-16'>
+             <ul className='flex gap-4 items-center justify-end sm:pb-0 pb-3 text-lg cursor-pointer sm:pr-12'>
             <li>Cart</li>
             <li><FaShoppingCart /></li>
             <li className='bg-fuchsia-700 text-white rounded-full px-2'>{cartVal}</li>
